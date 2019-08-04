15d71f24088f7097a92bf6a5880d9efc4d8f2dd1
cruel-32 <cruel32@nate.com>
2019-08-04 17:22:02 +0900

    dashboard member detail 구현중

--- src/views/Dashboard/DashboardMemberDetail.vue
+++ src/views/Dashboard/DashboardMemberDetail.vue
@@ -36,9 +36,38 @@ grad<template>
                       </v-btn>
                     </v-flex>
 
+                    <!-- <v-range-slider
+                      v-model="range"
+                      :max="max"
+                      :min="min"
+                      hide-details
+                      class="align-center"
+                    >
+                      <template v-slot:prepend>
+                        <v-text-field
+                          v-model="range[0]"
+                          class="mt-0 pt-0"
+                          hide-details
+                          single-line
+                          type="number"
+                          style="width: 60px"
+                        ></v-text-field>
+                      </template>
+                      <template v-slot:append>
+                        <v-text-field
+                          v-model="range[1]"
+                          class="mt-0 pt-0"
+                          hide-details
+                          single-line
+                          type="number"
+                          style="width: 60px"
+                        ></v-text-field>
+                      </template>
+                    </v-range-slider> -->
+
 
                     <v-flex xs12 sm6 md6>
-                      <v-subheader>처음 참여일</v-subheader>
+                      <v-subheader>최근</v-subheader>
                       <div id="chart"></div>
                     </v-flex>
 
@@ -157,7 +186,6 @@ export default class DashboardMemberDetail extends Vue {
     }
     return recentPartiDate
   }
-
 
   //차트 관련
   chart:any;
